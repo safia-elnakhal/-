4e39d670633f7ff805a4c9aa14db14c58b9aa44e
safia <safiaelnakhal@gmail.com>
2021-12-29 15:07:43 +0200

    update

---
+++ js/index.js
@@ -0,0 +1,31 @@
+// var inputName = document.getElementById("inputName");
+// var inputEmail = document.getElementById("inputEmail");
+// var inputKind = document.getElementById("inputKind");
+// var inputAdress = document.getElementById("inputAdress");
+// var inputDetails = document.getElementById("inputDetails");
+
+// var productsList ;//lma y3ml refresh hyfdaaa
+// if(localStorage.getItem("myProducts") == null )//zbon gdid malo4 7aga
+// {
+//     productsList = [];
+// }
+// else
+// {
+//     productsList = JSON.parse( localStorage.getItem("myProducts") );
+//     displayProducts();
+//     //productList malyaan bel7aga bta3 embar7
+// }
+// function addProduct() {
+
+//     var product =
+//     {
+//         name: productNameInp.value,
+//         price: productPriceInp.value,
+//         category: productCategoryInp.value,
+//         desc: productDescInp.value,
+//     }
+//     productsList.push(product);//product dllwa2y w products embar7 
+//     localStorage.setItem("myProducts" , JSON.stringify( productsList ) );
+//     displayProducts();
+//     clearForm();
+// }
--- reg.php
+++ reg.php
@@ -1,4 +1,7 @@
-<?php
+
+<html>
+<body>
+  <?php
 $conn=mysqli_connect('127.0.0.1','root','','test');
 
 if (!$conn) {
@@ -8,13 +11,27 @@ if (!$conn) {
 }
 else{echo 'connected';}
 
-$name =$_POST['name'];
-$email =$_POST['email'];
-$kind =$_POST['kind'];
-$adress =$_POST['adress'];
-$details =$_POST['details'];
+ if ($_SERVER["REQUEST_METHOD"] == "POST") { ?>
+  Welcome <?php echo $_POST["name"]; ?><br>
+  Your email address is: <?php echo $_POST["email"]; ?>
+  and kind <?php echo $_POST['kind'];?>
+  and adress <?php echo $_POST['adress'];?>
+  and details <?php echo $_POST['details'];
 
-$sql="INSERT INTO user(name,email,kind,adress,details) values('$name' ,'$email', '$kind','$adress','$details')";
+?>
+  <?php 
+}
+        else{ ?>
+   <script> location.replace("yourHtmlFileName.html") </script>
+   <?php     } 
+
+// $name=$_POST['name'];
+// $email=$_POST['email'];
+// $kind=$_POST['kind'];
+// $adress=$_POST['adress'];
+// $details=$_POST['details'];
+
+$sql="INSERT INTO user(name,email,kind,adress,details) values('name' ,'email', 'kind','adress','details')";
 
 if (!mysqli_query($conn ,$sql)){
 
@@ -23,4 +40,8 @@ if (!mysqli_query($conn ,$sql)){
 }
 
 else{echo 'Inserted';}
-?>
+?>  
+</body>
+</html> 
+
+    
